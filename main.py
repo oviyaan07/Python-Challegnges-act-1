8487bbe627b4500d99814346e6a93b0eb76d5046
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-16 12:24:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y
+print (z)
+a=v+w*x/y
+print (a)
+name="ALEX"
+age=0
+if name == "ALEX" or name == "John" and age>=2:
+    print ("welcome")
+else:
+    print ("goodbye")
